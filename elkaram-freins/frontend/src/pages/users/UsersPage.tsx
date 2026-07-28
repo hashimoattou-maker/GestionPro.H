393611691b96b3c64e6084e11469896e309a3381
Hashim El Karam <hashim@ahmedelkaram.com>
2026-07-28 18:28:06 +0200

    Remove edit button from users page

--- elkaram-freins/frontend/src/pages/users/UsersPage.tsx
+++ elkaram-freins/frontend/src/pages/users/UsersPage.tsx
@@ -184,9 +184,6 @@ export default function UsersPage() {
                       </TableCell>
                       <TableCell>
                         <div className="flex items-center gap-1">
-                          <Button variant="ghost" size="icon" onClick={() => openEdit(user)}>
-                            <Edit className="h-4 w-4" />
-                          </Button>
                           <Button variant="ghost" size="icon" className="text-red-600" onClick={() => setDeleteId(user.id)}>
                             <Trash2 className="h-4 w-4" />
                           </Button>
